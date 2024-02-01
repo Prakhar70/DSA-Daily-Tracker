3cb02373e338b5351583265da758c0311b1e685c
Prakhar70 <42569845+Prakhar70@users.noreply.github.com>
2024-02-01 07:53:50 +0530

    Detech Loop in LL

---
+++ LinkedList/DetechLoop/DetechLoop.java
@@ -0,0 +1,19 @@
+package LinkedList.DetechLoop;
+
+import LinkedList.ListNode;
+
+public class DetechLoop {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next != null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if (slow==fast){
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
